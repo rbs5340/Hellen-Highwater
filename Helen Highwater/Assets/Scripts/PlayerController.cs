4eba9a408f30044f65bcbb29867b7a17b52296a4
djkas-rit <djk2529@rit.edu>
2025-02-05 04:09:38 -0500

    Setup Rewired input manager and refactor player controller

--- Helen Highwater/Assets/Scripts/PlayerController.cs
+++ Helen Highwater/Assets/Scripts/PlayerController.cs
@@ -1,76 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using Rewired;
 
-public class Playercontroller: MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpStrength;
-    public Vector2 position;
-    public Vector2 velocity;
-    private bool moving;
-    private float direction;
+    public int playerId = 0; // The Rewired player ID (for a single player game should always be 0)
+    private Rewired.Player player; // The Rewired Player
 
     private Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpStrength = 10f;
+    public float decelerationFactor = 0.9f;
+
+    [Header("Attack Settings")]
+    public GameObject WrenchPrefab;
+    public Transform attackSpawnPoint;
+
+    private bool isGrounded;
+    private float direction;
+
+    private void Start()
     {
+        // Get Rewired Player
+        player = ReInput.players.GetPlayer(playerId);
+
+        // Get Rigidbody2D
         rb = GetComponent<Rigidbody2D>();
-        //velocity = new Vector2(0, 0);
+
+        if (!rb)
+        {
+            Debug.LogError("No Rigidbody2D found on Player!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        /*
-        
-        if  (Input.GetKey(KeyCode.LeftArrow))
-        {
-            velocity.x = -1 * moveSpeed;
-            Debug.Log("LEFT DOWN");
-        }
-        
-        
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            velocity.x = 1 * moveSpeed;
-            Debug.Log("RIGHT DONW");
-        }
+        HandleMovement();
+        HandleJumping();
+        HandleAttack();
+    }
 
-        //Slowly stop the player after inputs are done
-        else
-        {
-            if(Mathf.Abs(velocity.x) > 0)
-            {
-                velocity.x *= 0.99f;
-                Debug.Log("NMOOO");
-            }
-            
-            
-        }
-        //velocity.y = rb.velocity.y;
-        position += velocity;
-        transform.position = position;
-        */
-        
-        direction = Input.GetAxis("Horizontal");
-        if (direction > 0f || direction < 0f)
+    private void HandleMovement()
+    {
+        direction = player.GetAxis("MoveHorizontal");
+
+        if (Mathf.Abs(direction) > 0.1f) // small dead zone to prevent jitter
         {
             rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
         }
         else
         {
-            
-            rb.velocity = new Vector2(0,rb.velocity.y);
-            
+            // apply deceleration
+            rb.velocity = new Vector2(rb.velocity.x * decelerationFactor, rb.velocity.y);
         }
+    }
 
-        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+    private void HandleJumping()
+    {
+        if (player.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+            isGrounded = false; // prevent jumping in midair
         }
+    }
 
+    private void HandleAttack()
+    {
+        if (player.GetButtonDown("Attack") && WrenchPrefab && attackSpawnPoint)
+        {
+            Instantiate(WrenchPrefab, attackSpawnPoint.position, attackSpawnPoint.rotation);
+        }
+    }
 
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
