7260044964565e9fa42cfcee50192603d7469134
Jason Weinberg (RIT Student) <jpw3667@rit.edu>
2025-02-06 13:44:03 -0500

    helen player state machine base

--- Helen Highwater/Assets/Scripts/PlayerController.cs
+++ Helen Highwater/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Playercontroller: MonoBehaviour
@@ -10,49 +11,93 @@ public class Playercontroller: MonoBehaviour
     public Vector2 velocity;
     private bool moving;
     private float direction;
+    private state playerState;
 
+
+    //Player states for all actions so far. i have a different rising and falling state in case we want to change the gravity to make the platforming feel better,
+    enum state
+    {
+        idle,
+        run,
+        rise,
+        fall,
+        damaged,
+        wrenchThrow,
+        dash
+
+    }
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerState = state.idle;
         //velocity = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        
-        if  (Input.GetKey(KeyCode.LeftArrow))
-        {
-            velocity.x = -1 * moveSpeed;
-            Debug.Log("LEFT DOWN");
-        }
-        
-        
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            velocity.x = 1 * moveSpeed;
-            Debug.Log("RIGHT DONW");
+
+        switch (playerState) {
+            case state.idle:
+                if(rb.velocity.y > 0)
+                {
+                    playerState = state.rise;
+                }
+                else if(rb.velocity.y < 0)
+                {
+                    playerState = state.fall;
+                }
+                if (rb.velocity.x != 0)
+                {
+                    playerState = state.run;
+                }
+
+                break;
+            case state.run:
+                if (rb.velocity.y > 0)
+                {
+                    playerState = state.rise;
+                }
+                else if (rb.velocity.y < 0)
+                {
+                    playerState = state.fall;
+                }
+                if (rb.velocity.x == 0)
+                {
+                    playerState = state.idle;
+                }
+                break;
+            case state.rise:
+                if (rb.velocity.y == 0)
+                {
+                    playerState = state.idle;
+                }
+                else if(rb.velocity.y < 0)
+                {
+                    playerState = state.fall;
+                }
+                break;
+
+            case state.fall:
+                if (rb.velocity.y == 0)
+                {
+                    playerState = state.idle;
+                }
+                break;
+
+            case state.damaged:
+
+            case state.wrenchThrow:
+
+            case state.dash:
+
+
+            default:
+                break;
         }
 
-        //Slowly stop the player after inputs are done
-        else
-        {
-            if(Mathf.Abs(velocity.x) > 0)
-            {
-                velocity.x *= 0.99f;
-                Debug.Log("NMOOO");
-            }
-            
-            
-        }
-        //velocity.y = rb.velocity.y;
-        position += velocity;
-        transform.position = position;
-        */
-        
         direction = Input.GetAxis("Horizontal");
         if (direction > 0f || direction < 0f)
         {
@@ -60,17 +105,47 @@ public class Playercontroller: MonoBehaviour
         }
         else
         {
-            
-            rb.velocity = new Vector2(0,rb.velocity.y);
-            
-        }
 
+            rb.velocity = new Vector2(0, rb.velocity.y);
+
+        }
         if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
         }
 
+        Debug.Log(playerState.ToString());
 
 
+        /*
+
+if  (Input.GetKey(KeyCode.LeftArrow))
+{
+    velocity.x = -1 * moveSpeed;
+    Debug.Log("LEFT DOWN");
+}
+
+
+else if (Input.GetKey(KeyCode.RightArrow))
+{
+    velocity.x = 1 * moveSpeed;
+    Debug.Log("RIGHT DONW");
+}
+
+//Slowly stop the player after inputs are done
+else
+{
+    if(Mathf.Abs(velocity.x) > 0)
+    {
+        velocity.x *= 0.99f;
+        Debug.Log("NMOOO");
+    }
+
+
+}
+//velocity.y = rb.velocity.y;
+position += velocity;
+transform.position = position;
+*/
     }
 }
